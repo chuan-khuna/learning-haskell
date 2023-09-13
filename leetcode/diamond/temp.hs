85b91e2ddf32f468d9cac03eb45a48512bab7257
ALTR <chuan.khuna@gmail.com>
2023-09-13 23:24:56 +0700

    .

--- leetcode/diamond/temp.hs
+++ leetcode/diamond/temp.hs
@@ -4,17 +4,34 @@
 --     | (x - 2 * k > 0) && (x - 2 * k) > k = ([k] ++ [x - 2 * k] ++ [k]) : extend [x] (succ k)
 --     | otherwise = [[x]]
 
-f :: [Int] -> Int -> [[Int]]
-f [] k = [[]]
-f [x] k
-    | (x - 2 * k > 0) && (x - 2 * k) > k = ([k] ++ [x - 2 * k] ++ [k]) : f [x] (succ k)
+-- extend side
+-- prop edge to edge
+prop :: [Int] -> Int -> [[Int]]
+prop [] k = [[]]
+prop [x] k
+    | (x - 2 * k > 0) && (x - 2 * k) > k = [result] ++ resultRec ++ prop [x] (succ k)
     | otherwise = [[x]]
-f (x : xs) k
-    | head arr - k > k = ([k] ++ [head arr - k] ++ init (tail arr) ++ [last arr - k] ++ [k]) : f arr (succ k)
+  where
+    result = [k] ++ [x - 2 * k] ++ [k]
+    -- recursive call to the result
+    resultRec = filter (not . null) (prop result 1)
+prop (x : xs) k
+    | head arr - k > k = [result] ++ resultRec ++ prop arr (succ k)
     | otherwise = [[]]
   where
     arr = x : xs
+    result = [k] ++ [head arr - k] ++ init (tail arr) ++ [last arr - k] ++ [k]
+    resultRec = filter (not . null) (prop result 1)
 
-filter (not . null) (concat (map (`f` 1) (f [10] 1)))
+-- filter (not . null) (concatMap (`prop` 1) (prop [10] 1))
 
--- concatMap (`f` 1) (f [10] 1)
+splitList arr
+    -- check if odd
+    | length arr `mod` 2 == 1 = [a, [arr !! half], d]
+    | otherwise = [a, b]
+  where
+    half = length arr `div` 2
+    -- half for even
+    (a, b) = splitAt half arr
+    -- (a, d)  for odd
+    (c, d) = splitAt (half + 1) arr
