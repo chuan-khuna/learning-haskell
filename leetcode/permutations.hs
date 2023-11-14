7d4f62073b1d46c669de77e492e9b3019febc3cd
ALTR <chuan.khuna@gmail.com>
2023-11-14 22:00:30 +0700

    add permutation II

--- leetcode/permutations.hs
+++ leetcode/permutations.hs
@@ -1,8 +1,13 @@
 -- leetcode 46: Permutations
--- copilot generated answer
+-- leetcode 47: Permutations II
 
 import Data.List
 
+-- copilot generated answer
 permute :: (Ord a) => [a] -> [[a]]
 permute [] = [[]]
 permute xs = [x : ys | x <- xs, ys <- permute (delete x xs)]
+
+permuteII :: [Int] -> [[Int]]
+permuteII [] = [[]]
+permuteII xs = [x : ys | x <- nub xs, ys <- permute (delete x xs)]
