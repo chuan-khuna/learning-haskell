c5d1b900bb7b84d69b831963e55552e09a50a3c6
ALTR <chuan.khuna@gmail.com>
2023-11-15 22:20:08 +0700

    add comment

--- leetcode/frogJump.hs
+++ leetcode/frogJump.hs
@@ -8,10 +8,12 @@ canCross stones = solve stones 0 1
   where
     solve :: [Int] -> Int -> Int -> Bool
     -- solve stones value k
+    -- check if `value` + k (this jump) can reach the last stone or in the `stones` list
     solve stones value k
         | currentValue == last stones = True
         | currentValue > last stones = False
         | currentValue `notElem` stones = False
         | otherwise = any (solve stones currentValue) (filter (> 0) [k - 1, k, k + 1])
       where
+        -- jump to the next stone(or water?)
         currentValue = value + k
