da5d6eda1fec845f0a17c7ef4301ddb3a07836f6
ALTR <chuan.khuna@gmail.com>
2023-11-16 08:27:47 +0700

    find duplicate using map

--- leetcode/findDuplicate.hs
+++ leetcode/findDuplicate.hs
@@ -5,6 +5,10 @@
 -- const arr = [1, 1, 2, 3, 4, 5, 6, 7, 8, 6, 6, 7, 7, 7, 10, 10]; 
 -- console.log(returnMultipleDupesArray(arr)); // prints [1, 6, 7, 10]
 
+-- https://leetcode.com/problems/find-all-duplicates-in-an-array/description/
+
+import Data.Map as M
+
 findDuplicate :: [Int] -> [Int]
 findDuplicate [] = []
 findDuplicate (x:xs)
@@ -17,3 +21,13 @@ findDup [] dupList = dupList
 findDup (x:xs) dupList
     | (x `elem` xs) && notElem x dupList = findDup xs (x:dupList)
     | otherwise = findDup xs dupList
+
+findDupMap :: [Int] -> [Int]
+findDupMap arr = L.map (\(x, y) -> x) $ L.filter (\(x, y) -> y > 1) $ M.toList map
+    where
+        createMap :: [Int] -> Map Int Int -> Map Int Int
+        createMap [] map = map
+        createMap (x:xs) map
+            | M.lookup x map == Nothing = createMap xs (M.insert x 1 map)
+            | otherwise = createMap xs (M.adjust (+1) x map)
+        map = createMap arr M.empty
