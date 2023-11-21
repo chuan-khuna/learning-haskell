f860c9e03c07b6bddbfbd626dd84fa7fc55dfb98
ALTR <chuan.khuna@gmail.com>
2023-11-21 08:50:00 +0700

    fixed typo

--- leetcode/freqMostFrequentElem.hs
+++ leetcode/freqMostFrequentElem.hs
@@ -1,12 +1,14 @@
 -- https://leetcode.com/problems/frequency-of-the-most-frequent-element/description/
 
-increseToElement :: [Int] -> Int -> Int -> [Int]
+import Data.List as List
+
+increaseToElem :: [Int] -> Int -> Int -> [Int]
 -- nums, target, k, result nums
-increseToElement [] _ _ = []
-increseToElement (x : xs) target k
-    | x == target = x : increseToElement xs target k
-    -- be able to increate x, becase the gap is smaller than k
-    | (x < target) && (target - x <= k) = target : increseToElement xs target (k - (target - x))
+increaseToElem [] _ _ = []
+increaseToElem (x : xs) target k
+    | x == target = x : increaseToElem xs target k
+    -- be able to increase x, because the gap is smaller than k
+    | (x < target) && (target - x <= k) = target : increaseToElem xs target (k - (target - x))
     | otherwise = x : xs
 
 filterLessThanTarget :: [Int] -> Int -> [Int]
@@ -19,10 +21,10 @@ solve arr k = maxCount
     -- [[Int]], each [Int] is a filtered array, which is less than `target` (maximum of the array)
     -- then increase each element to target as many as possible base on `k`
 
-    -- targets = map (\filteredArr -> increseToElement filteredArr (maximum filteredArr) k) (map (filterLessThanTarget sortedArr) sortedArr)
+    -- targets = map (\filteredArr -> increaseToElem filteredArr (maximum filteredArr) k) (map (filterLessThanTarget sortedArr) sortedArr)
 
     -- suggested via warning
-    targets = map ((\filteredArr -> increseToElement filteredArr (maximum filteredArr) k) . filterLessThanTarget sortedArr) sortedArr
+    targets = map ((\filteredArr -> increaseToElem filteredArr (maximum filteredArr) k) . filterLessThanTarget sortedArr) sortedArr
 
     countTargets = map (\arr -> length $ filter (== maximum arr) arr) targets
     maxCount = maximum countTargets
@@ -32,9 +34,9 @@ solveOptimised arr k = solveHelper (reverse (List.sort arr)) k 0
   where
     solveHelper :: [Int] -> Int -> Int -> Int
     -- arr, k, maxCount -> maxCount
-    solveHelper [] _ _ = 0
+    solveHelper [] _ maxCount = maxCount
     solveHelper (x : xs) k maxCount = solveHelper xs k newMaxCount
       where
-        targetArr = increseToElement (x : xs) x k
+        targetArr = increaseToElem (x : xs) x k
         count = length $ filter (== x) targetArr
         newMaxCount = max maxCount count
