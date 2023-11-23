1062a0fff08c6094c3c6c8dea2347e41bf6a3e00
ALTR <chuan.khuna@gmail.com>
2023-11-23 16:10:13 +0700

    solve nQueen

--- leetcode/nQueens.hs
+++ leetcode/nQueens.hs
@@ -6,16 +6,27 @@ cartProd n = [(x, y) | x <- [0 .. n - 1], y <- [0 .. n - 1]]
 eliminate :: [(Int, Int)] -> (Int, Int) -> [(Int, Int)]
 eliminate coords (x, y) = filter (\(a, b) -> a /= x && b /= y && abs (a - x) /= abs (b - y)) coords
 
-nQueens :: Int -> [[(Int, Int)]]
-nQueens n = filter (\queens -> length queens == n) $ map (\queenCoord -> nQueens' (eliminate coords queenCoord) [queenCoord]) firstRowCoords
+-- nQueens :: Int -> [[(Int, Int)]]
+-- nQueens n = filter (\queens -> length queens == n) $ map (\queenCoord -> nQueens' (eliminate coords queenCoord) [queenCoord]) firstRowCoords
+--   where
+--     coords = cartProd n
+--     firstRowCoords = [(0, y) | y <- [0 .. n - 1]]
+--     -- coords, queens, return queen locations
+--     nQueens' :: [(Int, Int)] -> [(Int, Int)] -> [(Int, Int)]
+--     nQueens' [] queens = queens
+--     nQueens' coords queens = nQueens' remainingCoords' queens'
+--       where
+--         (x, y) = head coords
+--         remainingCoords' = eliminate coords (x, y)
+--         queens' = (x, y) : queens
+
+nQueens' :: [(Int, Int)] -> [(Int, Int)] -> [[(Int, Int)]]
+nQueens' coords queens
+    | null coords = [queens]
+    | otherwise = concat [nQueens' (eliminate coords (x, y)) (queens ++ [(x, y)]) | (x, y) <- firstRowCoords]
   where
-    coords = cartProd n
-    firstRowCoords = [(0, y) | y <- [0 .. n - 1]]
-    -- coords, queens, return queen locations
-    nQueens' :: [(Int, Int)] -> [(Int, Int)] -> [(Int, Int)]
-    nQueens' [] queens = queens
-    nQueens' coords queens = nQueens' remainingCoords' queens'
-      where
-        (x, y) = head coords
-        remainingCoords' = eliminate coords (x, y)
-        queens' = (x, y) : queens
+    firstRemainingRow = minimum [x | (x, y) <- coords]
+    firstRowCoords = filter (\(x, y) -> x == firstRemainingRow) coords
+
+n = 8
+filter (\x -> length x == n) (nQueens' (cartProd n) [])
